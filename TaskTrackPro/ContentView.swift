1b863d85936e93ee7fa0b5a4aee35cb41d6f9a81
PhoonThetPine <thetpine254@gmail.com>
2023-12-31 15:06:25 +0800

    Initial Commit

---
+++ TaskTrackPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskTrackPro
+//
+//  Created by Phoon Thet Pine on 31/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskTrackPro/TaskTrackProApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTrackProApp.swift
+//  TaskTrackPro
+//
+//  Created by Phoon Thet Pine on 31/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskTrackProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
