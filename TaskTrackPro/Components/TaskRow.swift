c3ba7f61aac14d0965dddfa53f420adf686a9fb0
PhoonThetPine <thetpine254@gmail.com>
2024-01-01 17:33:46 +0800

    TaskTrackPro - V2

--- TaskTrackPro/Components/TaskRow.swift
+++ TaskTrackPro/Components/TaskRow.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct TaskRow: View {
+    var task: String
+    var completed: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 20) {
+            Image(systemName: completed ? "checkmark.circle" : "circle")
+            
+            Text(task)
+        }
     }
 }
 
 #Preview {
-    TaskRow()
+    TaskRow(task: "Do laundry", completed: true)
 }
--- TaskTrackPro/Views/AddTaskView.swift
+++ TaskTrackPro/Views/AddTaskView.swift
@@ -8,11 +8,45 @@
 import SwiftUI
 
 struct AddTaskView: View {
+    
+    // MARK: - PROPERTIES
+    @EnvironmentObject var realmManager: RealmManager
+    @State private var title: String = ""
+    @Environment(\.dismiss) var dismiss
+    
+    // MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Create a new task")
+                .font(.title3).bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            TextField("Enter your task here", text: $title)
+                .textFieldStyle(.roundedBorder)
+            
+            Button {
+                if title != "" {
+                    realmManager.addTask(taskTitle: title)
+                }
+                dismiss()
+            } label: {
+                Text("Add task")
+                    .foregroundColor(.white)
+                    .padding()
+                    .padding(.horizontal)
+                    .background(Color(hue: 0.328, saturation: 0.796, brightness: 0.408))
+                    .cornerRadius(30)
+            }
+            Spacer()
+        }
+        .padding(.top, 40)
+        .padding(.horizontal)
+        .background(Color(hue: 0.086, saturation: 0.141, brightness: 0.972))
     }
 }
 
+// MARK: - PREVIEW
 #Preview {
     AddTaskView()
+        .environmentObject(RealmManager())
 }
